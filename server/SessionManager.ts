9744aa93f4baf67cb29b908a296aebff33c78ca8
Shivam Ramkisoen <s.r.v.ramkisoen@umail.leidenuniv.nl>
2021-12-20 00:27:44 +0100

    Refactor redis setup

--- server/SessionManager.ts
+++ server/SessionManager.ts
@@ -9,19 +9,20 @@ export default class SessionProvider {
     private redisStore: connectRedis.RedisStore;
     private redisClient: redis.RedisClient;
 
-    constructor(sessionHost: string, sessionPort: number) {
+    constructor() {
         this.redisStore = connectRedis(session);
         this.redisClient = redis.createClient({
-            host: sessionHost,
-            port: sessionPort,
+            host: process.env.REDIS_HOST || '127.0.0.1',
+            port: parseInt(process.env.REDIS_PORT || '6379'),
         });
+        this.connect()
     }
-    public connect(): void {
+    private connect(): void {
         this.redisClient.on('error', err => {
             consola.error('Redis error: ', err);
         });
         this.redisClient.on('ready', () => {
-            consola.ready('Initialized Redis')
+            consola.ready('Redis initialised');
         })
     }
     public getStore(): connectRedis.RedisStore {
