7abd1c38dd50f3e901c20964d7a0bd10a77dec27
Shivam Ramkisoen <s.r.v.ramkisoen@umail.leidenuniv.nl>
2021-12-20 00:17:15 +0100

    Add title and description for embed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'oth-verification',
+    title: 'osu! Tournament Hub Verification',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Verify your account easily using your osu! and Discord accounts for access to the osu! tournament hub.' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
