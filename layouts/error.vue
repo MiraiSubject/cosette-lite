c8267cb1b79329d8def784885d056580ab4a0294
Shivam Ramkisoen <s.r.v.ramkisoen@umail.leidenuniv.nl>
2022-03-25 12:16:14 +0100

    Add custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="start">
+    <div>
+      <h1>{{ error.message }}</h1>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    statusCode() {
+      return (this.error && (this.error as any).statusCode) || 500;
+    },
+    message() {
+      return (this.error as any).message;
+    },
+  },
+});
+
+interface errorMessage {
+  statusCode: number;
+  message:string;
+}
+</script>
