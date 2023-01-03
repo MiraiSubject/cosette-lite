fd3b3badf75917f8a5e496c914a1bb4ac1ce62d9
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2023-01-03 20:54:20 +0100

    Fix if-statement in discord, add config support back

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,20 @@
+import type { ITournamentConfig } from '$lib/config.interface';
+import { readFile } from 'fs/promises';
 import type { PageServerLoad } from './$types';
- 
-export const load = (async () => {
-  return {
-    name: "osu! Tournament Hub",
-  };
+
+export const load = (async ({ locals }) => {
+  const res: ITournamentConfig = JSON.parse(await readFile('./config.json', 'utf-8'));
+
+  const output: {
+    name: string,
+    session?: SessionData
+  } = {
+    name: res.name
+  }
+
+  if (locals.session) {
+    output.session = locals.session.data
+  }
+
+  return output;
 }) satisfies PageServerLoad;
--- src/routes/checks/discord/+page.server.ts
+++ src/routes/checks/discord/+page.server.ts
@@ -1,6 +1,11 @@
+import type { ITournamentConfig } from '$lib/config.interface';
+import { readFile } from 'fs/promises';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = (async ({ locals }) => {
+
+  const res: ITournamentConfig = JSON.parse(await readFile('./config.json', 'utf-8'));
+
   if (!locals.session.data.osu) {
     return {
       status: 302,
@@ -13,6 +18,7 @@ export const load: PageServerLoad = (async ({ locals }) => {
   return {
     username: locals.session.data.osu.username,
     osuId: locals.session.data.osu.id,
-    roles: ["Verified"],
+    roles: res.discord.roles.map(item => item.name),
+    session: locals.session.data
   }
 }) satisfies PageServerLoad;
