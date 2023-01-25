3ff39a303f85a681978c976360b835bd163e3d73
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2023-01-25 22:44:19 +0100

    Remove unnecessary sequence and determine whether this is suitable for startup logic

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,20 +1,10 @@
 import { handleSession } from 'svelte-kit-cookie-session';
 import { PRIVATE_COOKIE_SECRET } from '$env/static/private'
-import { sequence } from "@sveltejs/kit/hooks";
-import type { Handle } from "@sveltejs/kit";
 
-// You can do it like this, without passing a own handle function
+console.log("Hello?")
+
 export const first = handleSession({
-	// Optional initial state of the session, default is an empty object {}
-	// init: (event) => ({
-	// 	views: 0
-	// }),
 	secret: `${PRIVATE_COOKIE_SECRET}`
 });
 
-export const second = (async ({ event, resolve }) => {
-	// container.resolve(DiscordBot);
-	return await resolve(event);
-}) satisfies Handle
-
-export const handle = sequence(first, second);
+export const handle = first
