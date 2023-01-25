1697839e2f2f13e58993f43194bb532b22f8977a
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2023-01-25 22:43:10 +0100

    Add (incomplete) handling of role management for a user

--- src/routes/auth/discord/callback/+server.ts
+++ src/routes/auth/discord/callback/+server.ts
@@ -1,7 +1,8 @@
-import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from '$env/static/private';
+import { DISCORD_BOT_TOKEN, DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from '$env/static/private';
 import { PUBLIC_DISCORD_CALLBACK_URL } from '$env/static/public';
 import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { config } from '$lib/config';
 
 async function getOAuthTokens(code: string) {
     const url = 'https://discord.com/api/v10/oauth2/token';
@@ -46,6 +47,68 @@ async function getUserData(tokens: {
     }
 }
 
+interface DiscordData {
+    application: Application;
+    scopes: string[];
+    expires: string;
+    user: User;
+}
+interface Application {
+    id: string;
+    name: string;
+    icon: string;
+    description: string;
+    hook: boolean;
+    bot_public: boolean;
+    bot_require_code_grant: boolean;
+    verify_key: string;
+}
+interface User {
+    id: string;
+    username: string;
+    avatar: string;
+    discriminator: string;
+    public_flags: number;
+}
+
+async function joinDiscordServer(user: User, token: string, nickname: string): Promise<number> {
+    try {
+        const response = await fetch(`https://discord.com/api/guilds/${config.discord.guildId}/members/${user.id}`, {
+            body: JSON.stringify({
+                access_token: token
+            }),
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bot ${DISCORD_BOT_TOKEN}`
+            },
+        });
+
+        switch (response.status) {
+            case 201:
+                console.log(`Joined ${user.id} to server!`);
+                break;
+            case 204:
+                console.log(`User is already in the guild.`);
+                break;
+            case 400: {
+                console.log(`${user.id} - ${user.username} has reached maximum guilds, checking if user is present in guild...`);
+                //tRPC call to check whether guildmember exists
+                const exists = true;
+                if (exists) {
+                    break;
+                } else {
+                    return 1;
+                }
+            }
+        }
+
+        // Perform IPC (or tRPC) request to bot to add roles
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 
 // Write cookie for the state which will be used to compare later for the linked role stuff.
 export const GET = (async ({ url, locals }) => {
@@ -69,7 +132,10 @@ export const GET = (async ({ url, locals }) => {
 
     const tokens = await getOAuthTokens(code);
 
-    const meData = await getUserData(tokens);
+    const meData: DiscordData = await getUserData(tokens);
+
+    await joinDiscordServer(meData.user, tokens.access_token, locals.session.data.osu?.username || '');
+
     console.log(`Discord User joined: ${meData.user.id} - ${meData.user.username}`);
 
     throw redirect(302, '/done');
