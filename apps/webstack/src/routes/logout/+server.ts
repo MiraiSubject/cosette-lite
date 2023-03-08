ce85b1f6ad2eebce70e03f2e430708703c90ff76
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2023-03-08 23:47:53 +0100

    Satisfy some errors

--- apps/webstack/src/routes/logout/+server.ts
+++ apps/webstack/src/routes/logout/+server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import type { RequestHandler } from '../auth/$types';
+import type { RequestHandler } from './$types';
 
 export const GET = (async ({ locals }) => {
     await locals.session.destroy();
