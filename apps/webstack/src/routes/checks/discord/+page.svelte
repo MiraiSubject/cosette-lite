64d971668e62b59ede838dd63e65438bbfdedffc
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2023-03-08 23:47:53 +0100

    Satisfy some errors

--- apps/webstack/src/routes/checks/discord/+page.svelte
+++ apps/webstack/src/routes/checks/discord/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { ITournamentConfig } from '$lib/config.interface';
+	import type { ITournamentConfig } from 'config';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
