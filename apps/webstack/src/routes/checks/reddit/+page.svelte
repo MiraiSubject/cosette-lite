1f90e8bc5c85bc5024b46abc60f81c363ad336fd
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2023-03-16 19:58:15 +0100

    Add reddit frontend page

---
+++ apps/webstack/src/routes/checks/reddit/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="start">
+	<div class="user-info">
+		<p class="right-align">
+			Currently verifying {data.osuUsername || ''} ({data.osuId || ''}).
+			<a href="/logout"><strong>Log out?</strong></a>
+		</p>
+	</div>
+	<div>
+		<h1>Connect your Reddit account</h1>
+	</div>
+	<div>
+		<p>In this step, we will need some information about your Reddit account.</p>
+		<p>This information will be used to:</p>
+		<ul>
+			<li>Make sure your Reddit account was made before this years event began.</li>
+			<li>Change the nickname on the discord server to your Reddit username.</li>
+		</ul>
+		<p>
+			<strong
+				>Make sure you're on the correct Reddit account before accepting the prompt on their side.</strong
+			>
+		</p>
+	</div>
+	<a href="/auth/reddit" class="flex button reddit"> Log in with Reddit! </a>
+</div>
