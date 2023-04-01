054643df00d42da552d6dcacbdc156e3f8fe0661
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2023-04-01 23:35:36 +0200

    Add documentation for the function we want people to touch

--- apps/webstack/src/routes/auth/osu/callback/+server.ts
+++ apps/webstack/src/routes/auth/osu/callback/+server.ts
@@ -48,8 +48,16 @@ async function getUserData(tokens: {
     }
 }
 
-function isUserEligible(meData: OsuUser): boolean {
-    const joinDate = DateTime.fromISO(meData.join_date);
+/**
+    * @description This function is used to check if the user is eligible for the verified role(s) specfied in your config.
+    * The default is to check if the user is older than 6 months.
+    * You can modify the function to check for other things, such as if the user has a certain amount of playtime, rank or pp.
+    * @param userData The osu! profile of the user.
+    * @returns A boolean indicating if the user is eligible for the linked role.
+    * 
+*/
+function isUserEligible(userData: OsuUser): boolean {
+    const joinDate = DateTime.fromISO(userData.join_date);
     const nowMinus6Months = DateTime.now().minus({ months: 6 });
 
     return nowMinus6Months > joinDate;
