7324656d66c79ac8939f28b2ae554bef06197fd3
Shivam <803255+MiraiSubject@users.noreply.github.com>
2026-06-01 21:30:58 +0200

    Update dependencies for security patches

--- apps/webstack/vite.config.js
+++ apps/webstack/vite.config.js
@@ -1,19 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
 
-/** @type {import('vite').UserConfig} */
-const config = {
+export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	server: {
 		port: 8000,
-		strictPort: false,
+		strictPort: false
 	},
 	preview: {
 		port: 4173,
-		strictPort: false,
+		strictPort: false
 	}
-};
-
-export default config;
+});
